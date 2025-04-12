e81415172af1aaad56581c2489f55d47234f9c1f
egg-yolk-jpg <113297382+egg-yolk-jpg@users.noreply.github.com>
2025-04-12 15:58:24 -0500

    Create functions.py
    Hold functions to use in main.py

---
+++ functions.py
@@ -0,0 +1,7 @@
+############# Imports #############
+import random
+
+############# Importable Functions #############
+def get_bits_from_string(input):
+    return [random.randint(0, 1) for _ in range(len(input))]
+print("hi")
